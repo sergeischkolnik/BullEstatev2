4e7db5d4773b76c41256e4e8954f0c8a0fd776e8
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-01-15 21:10:24 -0300

    update pdf creator

--- pdfCreatorProyectos.py
+++ pdfCreatorProyectos.py
@@ -148,10 +148,10 @@ def createPdfReport(cliente, fileName, data, headers,operacion,resultado3):
 
     ptext = '<font size=12>Sergei Schkolnik</font>'
     Story.append(Paragraph(ptext, styles["Justify"]))
-    
+
     if len(resultado3)>0:
         for link in resultado3:
-            linkHtml = '<link href="' + link + '" color="blue">' + str(link) + '</link>'
+            linkHtml = '<link href="' + str(link) + '" color="blue">' + str(link) + '</link>'
             Story.append(Paragraph(linkHtml, styles["Normal"]))
             Story.append(linkHtml)
     doc.build(Story)
