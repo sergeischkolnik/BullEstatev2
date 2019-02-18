eae26c5435b61732694b6f57672826278b5bd11a
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-02-18 12:54:46 -0300

    cambios

--- clientes_propiedades_compra_arriendo.py
+++ clientes_propiedades_compra_arriendo.py
@@ -304,7 +304,6 @@ def calcularDistanciaV(i,data):
         #print("propiedades encontradas "+str(len(distancias)))
         #print ("nivel de confianza: "+str(t_actual))
         for link in [x[13] for x in distancias]:
-             print (link)
 
 
         # i3=op, i4=tipo, i5=precio, i6=dorms, i7=baños, i12= estacionamientos i8=util, i9=total
