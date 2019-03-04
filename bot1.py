7833ca46c7f3e84f3a975849c1c370bb962d246c
Demian-bullestate <demianschkolnik@gmail.com>
2019-03-04 10:14:20 -0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -20,7 +20,7 @@ comandosMultiples = ['reporte','tasador','tasadorlinks','banear']
 id_chats_updates = ["485728961","652659504"]
 
 def getClientesMailer():
-    sql="SELECT duenos.mail,duenos.comision,duenos.exclusividad,duenos.estado from duenos inner join portalinmobiliario where duenos.idProp=portalinmobiliario.id2 and estado IS NOT NULL"
+    sql="SELECT duenos.mail,duenos.comision,duenos.exclusividad,duenos.estado,portalinmobiliario.precio from duenos inner join portalinmobiliario where duenos.idProp=portalinmobiliario.id2 and estado IS NOT NULL"
     mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
     cur = mariadb_connection.cursor()
     cur.execute(sql)
@@ -33,6 +33,7 @@ def getClientesMailer():
         text += "Comision: " + str(elem[1]) + "%\n"
         text += "Exclusividad: " + str(elem[2]) + "\n"
         text += "Estado: " + str(elem[3]) + "\n"
+        text += "Precio Propiedad: $" + str(elem[4]) + " pesos\n"
         text += "\n"
 
     text += "Clientes activos del mailer: " + str(totalClients) + "\n"
