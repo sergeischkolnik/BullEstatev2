4e92b2a5dc8ffb18f4128c31f3a5119976aa3103
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-04-05 17:55:01 -0300

    Create rentabilidad_arriendo.py

---
+++ rentabilidad_arriendo.py
@@ -0,0 +1,50 @@
+import math
+import pymysql as mysql
+from math import radians, sin, cos, acos, asin,pi,sqrt
+from datetime import datetime, timedelta, date
+past = datetime.now() - timedelta(days=90)
+past=datetime.date(past)
+yesterday = datetime.now() - timedelta(days=30)
+yesterday=datetime.date(yesterday)
+from threading import Thread
+from time import sleep
+from datetime import datetime, timedelta
+import pdfCreatorPropiedadescompra_arriendo as pdfC
+import uf
+import numpy as np
+from sklearn import datasets, linear_model
+import sendmail
+import tasadorbot2 as tb2
+import pubPortalExiste
+import math
+
+def obtenertipos():
+
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+    cur = mariadb_connection.cursor()
+    sql = "SELECT tipo FROM portalinmobiliario"
+    cur.execute(sql)
+    tipos = cur.fetchall()
+    return tipos
+
+def obtenercomunas():
+
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+    cur = mariadb_connection.cursor()
+    sql = "SELECT link FROM portalinmobiliario"
+    cur.execute(sql)
+    links = cur.fetchall()
+    comunas=[]
+    for i in links:
+        j=i.split('/')
+        comuna=j[5]
+        comunas.append(comuna)
+    return comunas
+
+comunas=obtenercomunas()
+tipos=obtenertipos()
+dormitorios=[1,2,3,4,5]
+banos=[1,2,3,4,5]
+estacionamientos=[1,2,3]
+print(comunas)
+print(tipos)
