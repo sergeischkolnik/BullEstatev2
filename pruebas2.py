343e3215a0ccc483e121363abf5acb809ef1f190
Demian-bullestate <demianschkolnik@gmail.com>
2019-06-08 11:33:58 -0400

    Update pruebas2.py

--- pruebas2.py
+++ pruebas2.py
@@ -1,3 +1,30 @@
+import threading
+import time
 
-from datetime import datetime
-print(datetime.now().hour)
+
+
+def doit(arg):
+    t = threading.currentThread()
+    while getattr(t, "do_run", True):
+        print ("working on" + str(arg))
+        time.sleep(1)
+    print("Stopping as you wish.")
+
+t = threading.Thread(target=doit, args=(0,))
+
+def main():
+
+    for i in range(1,3):
+        print("main:" + str(i))
+        if(i==1):
+            t = threading.Thread(target=doit, args=(i,))
+            t.setDaemon(True)
+            t.start()
+
+        if(i==2):
+            t.do_run = False
+
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
