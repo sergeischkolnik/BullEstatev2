7ce68d4ac7849871f0379ed209f30891a85e84cb
Demian-bullestate <demianschkolnik@gmail.com>
2019-06-08 11:33:54 -0400

    Update mailBotClientesArriendoFrancisca.py

--- mailBotClientesArriendoFrancisca.py
+++ mailBotClientesArriendoFrancisca.py
@@ -1,4 +1,4 @@
-
+import threading
 import pymysql as mysql
 from datetime import datetime, timedelta
 import random
@@ -10,17 +10,9 @@ past=datetime.date(past)
 yesterday = datetime.now() - timedelta(days=2)
 yesterday=datetime.date(yesterday)
 
+sleepTime=random.randint(150,250)
 
-def checkClient(clientMail,comision):
-    sql = "UPDATE duenos SET contactado='si',comision='"+str(comision)+"' WHERE mail='"+str(clientMail)+"'"
-    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
-    cur = mariadb_connection.cursor()
-    cur.execute(sql)
-    mariadb_connection.commit()
-    mariadb_connection.close()
-
-def sendClientMailsDeptos():
-    sql = "select duenos.mail,portalinmobiliario.nombre,portalinmobiliario.link from duenos inner join portalinmobiliario where " \
+sqlDeptos = "select duenos.mail,portalinmobiliario.nombre,portalinmobiliario.link from duenos inner join portalinmobiliario where " \
           "duenos.idProp=portalinmobiliario.id2 and duenos.contactado IS NULL and " \
           "duenos.esDueno='si' and (portalinmobiliario.operacion='arriendo') and portalinmobiliario.tipo='departamento' and " \
           "portalinmobiliario.fechascrap>='"+str(yesterday)+"' and portalinmobiliario.fechapublicacion>'" + str(past) + "' and " \
@@ -36,10 +28,45 @@ def sendClientMailsDeptos():
           "(portalinmobiliario.link like '%nunoa%' and ((portalinmobiliario.dormitorios='1' and portalinmobiliario.precio<'930000') or portalinmobiliario.dormitorios>'1')) or "\
           "(portalinmobiliario.link like 'san-miguel' and ((portalinmobiliario.dormitorios='1' and portalinmobiliario.precio<'600000') or (portalinmobiliario.dormitorios>'1'))));"
 
+sqlCasas = "select duenos.mail,portalinmobiliario.nombre,portalinmobiliario.link from duenos inner join portalinmobiliario where " \
+      "duenos.idProp=portalinmobiliario.id2 and duenos.contactado IS NULL and " \
+      "duenos.esDueno='si' and portalinmobiliario.precio>'400000' and (portalinmobiliario.operacion='arriendo') and portalinmobiliario.tipo='casa' and " \
+      "portalinmobiliario.fechascrap>='" + str(yesterday) + "' and portalinmobiliario.fechapublicacion>'" + str(
+    past) + "' and " \
+            "(portalinmobiliario.link like '%lo-barnechea%' or " \
+            "portalinmobiliario.link like '%vitacura%' or " \
+            "portalinmobiliario.link like '%la-florida%' or " \
+            "portalinmobiliario.link like '%providencia%' or " \
+            "portalinmobiliario.link like '%nunoa%' or " \
+            "(portalinmobiliario.link like '%colina%' and ((portalinmobiliario.lat<'-33.264536' and portalinmobiliario.lat>'-33.308362' and portalinmobiliario.lon<'-70.618245' and portalinmobiliario.lon>'-70.699193') or (portalinmobiliario.lat<'-33.301430' and portalinmobiliario.lat>'-33.335555' and portalinmobiliario.lon<'-70.622641' and portalinmobiliario.lon>'-70.666652'))) or " \
+            "portalinmobiliario.link like '%maipu%' or " \
+            "portalinmobiliario.link like '%las-condes%' or portalinmobiliario.link like '%la-reina%');"
+
+sqlOficinas = "select duenos.mail,portalinmobiliario.nombre,portalinmobiliario.link from duenos inner join portalinmobiliario where " \
+      "duenos.idProp=portalinmobiliario.id2 and duenos.contactado IS NULL and " \
+      "duenos.esDueno='si' and (portalinmobiliario.operacion='arriendo') and (portalinmobiliario.tipo='oficina' or portalinmobiliario.tipo='comercial') and " \
+      "portalinmobiliario.fechascrap>='" + str(yesterday) + "' and portalinmobiliario.fechapublicacion>'" + str(
+    past) + "' and " \
+            "(portalinmobiliario.link like '%santiago-metropolitana%' or " \
+            "portalinmobiliario.link like '%lo-barnechea%' or " \
+            "portalinmobiliario.link like '%vitacura%' or " \
+            "(portalinmobiliario.link like '%huechuraba%' and ((portalinmobiliario.lat<'-33.374926' and portalinmobiliario.lat>'-33.396264' and portalinmobiliario.lon<'-70.603082' and portalinmobiliario.lon>'-70.630241'))) or " \
+            "portalinmobiliario.link like '%nunoa%' or " \
+            "portalinmobiliario.link like '%las-condes%' or portalinmobiliario.link like '%providencia%');"
+
+def checkClient(clientMail,comision):
+    sql = "UPDATE duenos SET contactado='si',comision='"+str(comision)+"' WHERE mail='"+str(clientMail)+"'"
     mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
-    print(sql)
     cur = mariadb_connection.cursor()
     cur.execute(sql)
+    mariadb_connection.commit()
+    mariadb_connection.close()
+
+def sendClientMailsDeptos():
+
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+    cur = mariadb_connection.cursor()
+    cur.execute(sqlDeptos)
     lista = cur.fetchall()
     mariadb_connection.close()
 
@@ -55,24 +82,13 @@ def sendClientMailsDeptos():
         mailer.sendMailGratis(to,nombreProp,linkProp)
         checkClient(to,"1")
 
-        time.sleep(random.randint(150,250))
+        time.sleep(sleepTime)
 
 def sendClientMailsCasas():
-    sql = "select duenos.mail,portalinmobiliario.nombre,portalinmobiliario.link from duenos inner join portalinmobiliario where " \
-          "duenos.idProp=portalinmobiliario.id2 and duenos.contactado IS NULL and " \
-          "duenos.esDueno='si' and portalinmobiliario.precio>'400000' and (portalinmobiliario.operacion='arriendo') and portalinmobiliario.tipo='casa' and " \
-          "portalinmobiliario.fechascrap>='"+str(yesterday)+"' and portalinmobiliario.fechapublicacion>'" + str(past) + "' and " \
-          "(portalinmobiliario.link like '%lo-barnechea%' or " \
-          "portalinmobiliario.link like '%vitacura%' or " \
-          "portalinmobiliario.link like '%la-florida%' or " \
-          "portalinmobiliario.link like '%providencia%' or " \
-          "portalinmobiliario.link like '%nunoa%' or " \
-          "(portalinmobiliario.link like '%colina%' and ((portalinmobiliario.lat<'-33.264536' and portalinmobiliario.lat>'-33.308362' and portalinmobiliario.lon<'-70.618245' and portalinmobiliario.lon>'-70.699193') or (portalinmobiliario.lat<'-33.301430' and portalinmobiliario.lat>'-33.335555' and portalinmobiliario.lon<'-70.622641' and portalinmobiliario.lon>'-70.666652'))) or " \
-          "portalinmobiliario.link like '%maipu%' or " \
-          "portalinmobiliario.link like '%las-condes%' or portalinmobiliario.link like '%la-reina%');"
+
     mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
     cur = mariadb_connection.cursor()
-    cur.execute(sql)
+    cur.execute(sqlCasas)
     lista = cur.fetchall()
     mariadb_connection.close()
 
@@ -86,22 +102,14 @@ def sendClientMailsCasas():
         mailer.sendMailGratis(to,nombreProp,linkProp)
         checkClient(to,"1")
 
-        time.sleep(random.randint(150,250))
+        time.sleep(sleepTime)
 
 def sendClientMailsOficinas():
-    sql = "select duenos.mail,portalinmobiliario.nombre,portalinmobiliario.link from duenos inner join portalinmobiliario where " \
-          "duenos.idProp=portalinmobiliario.id2 and duenos.contactado IS NULL and " \
-          "duenos.esDueno='si' and (portalinmobiliario.operacion='arriendo') and (portalinmobiliario.tipo='oficina' or portalinmobiliario.tipo='comercial') and " \
-          "portalinmobiliario.fechascrap>='"+str(yesterday)+"' and portalinmobiliario.fechapublicacion>'" + str(past) + "' and " \
-          "(portalinmobiliario.link like '%santiago-metropolitana%' or " \
-          "portalinmobiliario.link like '%lo-barnechea%' or " \
-          "portalinmobiliario.link like '%vitacura%' or " \
-          "(portalinmobiliario.link like '%huechuraba%' and ((portalinmobiliario.lat<'-33.374926' and portalinmobiliario.lat>'-33.396264' and portalinmobiliario.lon<'-70.603082' and portalinmobiliario.lon>'-70.630241'))) or " \
-          "portalinmobiliario.link like '%nunoa%' or " \
-          "portalinmobiliario.link like '%las-condes%' or portalinmobiliario.link like '%providencia%');"
+
+
     mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
     cur = mariadb_connection.cursor()
-    cur.execute(sql)
+    cur.execute(sqlOficinas)
     lista = cur.fetchall()
     mariadb_connection.close()
 
@@ -116,7 +124,67 @@ def sendClientMailsOficinas():
         mailer.sendMailGratis(to,nombreProp,linkProp)
         checkClient(to,"1")
 
-        time.sleep(random.randint(150,250))
+        time.sleep(sleepTime)
+
+def threadSendMails():
+    t = threading.currentThread()
+
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+    cur = mariadb_connection.cursor()
+
+    cur.execute(sqlDeptos)
+    listaDeptos = cur.fetchall()
+
+    cur.execute(sqlCasas)
+    listaCasas = cur.fetchall()
+
+    cur.execute(sqlOficinas)
+    listaOficinas = cur.fetchall()
+
+    mariadb_connection.close()
+
+    print("[" + str(datetime.now()) + "]Sending mails (dptos) to " + str(len(listaDeptos)) + " clients:")
+    for i,l in enumerate(listaDeptos):
+        if getattr(t, "do_run", False):
+            return "Detenido Bot Francisca - Arriendos"
+        to = str(l[0])
+        nombreProp = str(l[1])
+        linkProp=str(l[2])
+
+        #gratis
+        mailer.sendMailGratis(to,nombreProp,linkProp)
+        checkClient(to,"1")
+        time.sleep(sleepTime)
+
+    print("[" + str(datetime.now()) +"]Sending mails (casas) to "+str(len(listaCasas))+ " clients:")
+
+    for i,l in enumerate(listaCasas):
+        if getattr(t, "do_run", False):
+            return "Detenido Bot Francisca - Arriendos"
+        to = str(l[0])
+        nombreProp = str(l[1])
+        linkProp=str(l[2])
+
+        mailer.sendMailGratis(to,nombreProp,linkProp)
+        checkClient(to,"1")
+
+        time.sleep(sleepTime)
+
+    print("[" + str(datetime.now()) +"]Sending mails (oficinas) to "+str(len(listaOficinas))+ " clients:")
+
+    for i,l in enumerate(listaOficinas):
+        if getattr(t, "do_run", False):
+            return "Detenido Bot Francisca - Arriendos"
+        to = str(l[0])
+        nombreProp = str(l[1])
+
+        linkProp=str(l[2])
+
+        mailer.sendMailGratis(to,nombreProp,linkProp)
+        checkClient(to,"1")
+
+        time.sleep(sleepTime)
+
 
 sendClientMailsDeptos()
 sendClientMailsCasas()
