22f604ac77901af53c1061d8889fb409b1128cf6
Demian Schkolnik <demianschkolnik@gmail.com>
2019-06-09 16:22:40 -0400

    Update propManager.py

--- propManager.py
+++ propManager.py
@@ -1,28 +1,39 @@
 import pymysql as mysql
 
 def fetchFromTable(db,sql):
-	conn = mysql.connect(user='root', password='sergei', host = '127.0.0.1', database = db)
-	cur = conn.cursor()
-	cur.execute(sql)
-	return cur.fetchall()
+	try:
+		conn = mysql.connect(user='root', password='sergei', host = '127.0.0.1', database = db)
+		cur = conn.cursor()
+		cur.execute(sql)
+		return cur.fetchall()
+	except:
+		return -1
 
 def selectorPortal():
 	sql = "SELECT COUNT(id) from portalinmobiliario"
 	tupla = fetchFromTable("bullestate", sql)
-	num = int(tupla[0][0])
-	return num
+	if tupla != -1:
+		num = int(tupla[0][0])
+		return num
+	else:
+		return -1
 
 def selectorGP():
 	sql = "SELECT COUNT(id) from goplaceit_scraped"
 	tupla = fetchFromTable("goplaceit", sql)
-	num = int(tupla[0][0])
-	return num
+	if tupla != -1:
+		num = int(tupla[0][0])
+		return num
+	else:
+		return -1
 
 def getBotClients(hora):
 	sql = "select * from clientesBot where activo=1 and horaActualizacion="+str(hora)
-	print(sql)
 	clientes = fetchFromTable("bullestate",sql)
-	return clientes
+	if clientes != -1:
+		return clientes
+	else:
+		return -1
 
 def getProps(client):
 	select = "select link from portalinmobiliario"
@@ -31,5 +42,8 @@ def getProps(client):
 def getLastScrap():
 	sql = "select lastscrap from checker where id=1"
 	f = fetchFromTable("bullestate",sql)
-	return f[0][0]
+	if f != -1:
+		return f[0][0]
+	else:
+		return -1
 
