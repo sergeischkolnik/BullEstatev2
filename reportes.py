9ab07fa4fa987801ccae37ec2343cecb4e9f931a
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-06-19 10:34:24 -0400

    Update reportes.py

--- reportes.py
+++ reportes.py
@@ -997,8 +997,8 @@ def generarReporteInterno(preciomin, preciomax, utilmin, utilmax, totalmin, tota
                        operacion, region, comuna1, comuna2,
                        comuna3, comuna4, comuna5, comuna6,prioridad, flagMail, mail,nombreCliente,verboso):
 
-    preciomin=int(preciomin)
-    preciomax=int(preciomax)
+    preciomin=float(preciomin)
+    preciomax=float(preciomax)
     utilmin=int(utilmin)
     utilmax=int(utilmax)
     totalmin=int(totalmin)
