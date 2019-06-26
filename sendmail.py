c57f15ed388e09c0b0b7184efa5eaeabdf7b4899
Demian-bullestate <demianschkolnik@gmail.com>
2019-06-26 12:32:49 -0400

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -35,3 +35,36 @@ def sendMail(to,cliente,file):
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
+
+def sendMailMultiple(to,cliente,files):
+    fromaddr = "contacto@bullestate.cl"
+    toaddr = to
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Informe BullEstate"
+
+
+    body = "Estimado " + str(cliente) + " :\n\nAdjunto informe solicitado."
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    for file in files:
+        filename = file
+        attachment = open(file, "rb")
+
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+        msg.attach(part)
+
+    server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
+    #server.starttls()
+    server.login(fromaddr, "Bullestate.123")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
