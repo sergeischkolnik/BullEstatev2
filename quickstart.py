f6b9fd61a426a5721e6083b99073c35689ebbb57
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-07-05 18:44:52 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -6,7 +6,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
     """Shows basic usage of the Drive v3 API.
