b16b1481b88a372e63007e9207649d03b5b7c425
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-07-09 16:44:08 -0400

    Update sistemaGeneralReportesv1.py

--- sistemaGeneralReportesv1.py
+++ sistemaGeneralReportesv1.py
@@ -33,7 +33,7 @@ def main(verboso):
                                totalmin=cliente[9], totalmax=cliente[10], latmin=cliente[11], latmax=cliente[12], lonmin=cliente[13],
                                lonmax=cliente[14], dormitoriosmin=cliente[15], dormitoriosmax=cliente[16], banosmin=cliente[17],
                                banosmax=cliente[18], confmin=cliente[40], rentminventa=cliente[36],rentminarriendo=cliente[37],
-                               estacionamientos=cliente[19],bodegas=cliente[20], metrodistance=cliente[31],linea1=cliente[32],linea2=cliente[33],linea3=cliente[34],
+                               estacionamientos=cliente[19],bodegas=cliente[20], metrodistance=cliente[31],l1=cliente[32],l2=cliente[33],l3=cliente[34],
                                tipo=cliente[21], operacion=cliente[22],
                                region=cliente[24], listaComunas=comunas, prioridad=cliente[39], mail=cliente[3],
                                nombreCliente=cliente[1],idCliente=cliente[0], direccion=cliente[42], radioDireccion=cliente[43],corredor=cliente[41], verboso=verboso)
