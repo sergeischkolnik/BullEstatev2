edb813c3800ae59a3010d349a821dbc49753c4cc
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-07-24 16:50:48 -0400

    Update botEstates.py

--- botEstates.py
+++ botEstates.py
@@ -1,2 +1,58 @@
 def main():
+    estadosMain=['inicial']
+    estadosCanjeador=['inicial','operacion','tipo','region','comuna','preciomin','preciomax','utilmin','utilmax','terraza','dormitorios','banos','estacionamientos','bodegas','confirmar']
+    estadosFicha=['inicial','portal','id','confirmar']
 
+    comunasMetropolitana=[
+"buin",
+"calera de tango",
+"cerrillos",
+"cerro navia",
+"colina",
+"conchalí",
+"el bosque",
+"el monte",
+"estación central",
+"huechuraba",
+"independencia",
+"la cisterna",
+"la florida",
+"la granja",
+"la pintana",
+"la reina",
+"lampa",
+"las condes",
+"lo barnechea",
+"lo espejo",
+"lo prado",
+"macul",
+"maipú",
+"melipilla",
+"padre hurtado",
+"paine",
+"pedro aguirre cerda",
+"peñaflor",
+"peñalolén",
+"pirque",
+"providencia",
+"pudahuel",
+"puente alto",
+"quilicura",
+"quinta normal",
+"recoleta",
+"renca",
+"san bernardo",
+"san joaquín",
+"san miguel",
+"san pedro",
+"san ramón",
+"santiago",
+"talagante",
+"tiltil",
+"vitacura",
+"ñuñoa"]
+
+    print(len(comunasMetropolitana))
+
+if __name__ == '__main__':
+    main()
