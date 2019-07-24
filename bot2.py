692922b7c4333369cc05459e82060f0b0e3142d9
Demian-bullestate <demianschkolnik@gmail.com>
2019-07-24 11:32:15 -0400

    Create bot2.py

---
+++ bot2.py
@@ -0,0 +1,112 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler
+from telegram.ext import ConversationHandler, CallbackQueryHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+import logging
+
+telegram_token = "666842820:AAGg1F_NjlQBL7IPv9XlfMEC0PJ6iWlVLj0"
+
+LANG = "EN"
+SET_LANG, MENU, SET_STAT, REPORT, MAP, FAQ, ABOUT, LOCATION = range(8)
+STATE = SET_LANG
+
+def start(bot, update):
+    """
+    Start function. Displayed whenever the /start command is called.
+    This function sets the language of the bot.
+    """
+    # Create buttons to select language:
+    keyboard = [['ES', 'EN']]
+
+    # Create initial message:
+    message = "Hey, I'm DisAtBot! / ¡Hey, soy DisAtBot! \n\n\
+Please select a language to start. / Por favor selecciona un idioma \
+para comenzar."
+
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+    update.message.reply_text(message, reply_markup=reply_markup)
+
+    return SET_LANG
+
+
+def menu(bot, update):
+    """
+    Main menu function.
+    This will display the options from the main menu.
+    """
+    # Create buttons to slect language:
+    keyboard = [["Menu1", "menu2"],
+                ["menu3", "menu4"]]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+
+    user = update.message.from_user
+    logger.info("Menu command requested by {}.".format(user.first_name))
+    update.message.reply_text("mein menu66", reply_markup=reply_markup)
+
+    return SET_STAT
+
+def cancel(bot, update):
+    """
+    User cancelation function.
+    Cancel conersation by user.
+    """
+    user = update.message.from_user
+    logger.info("User {} canceled the conversation.".format(user.first_name))
+    update.message.reply_text("ADIOS",
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+def main():
+    """
+    Main function.
+    This function handles the conversation flow by setting
+    states on each step of the flow. Each state has its own
+    handler for the interaction with the user.
+    """
+    global LANG
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(telegram_token)
+
+    # Get the dispatcher to register handlers:
+    dp = updater.dispatcher
+
+    # Add conversation handler with predefined states:
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+
+            MENU: [CommandHandler('menu', menu)],
+
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel),
+                   CommandHandler('help', help)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # Log all errors:
+    dp.add_error_handler(error)
+
+    # Start DisAtBot:
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process
+    # receives SIGINT, SIGTERM or SIGABRT:
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
