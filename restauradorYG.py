9025043e06c571ddd34da166c8969ee6df091a9d
Demian-bullestate <demianschkolnik@gmail.com>
2019-07-29 16:18:12 -0400

    Create restauradorYG.py

---
+++ restauradorYG.py
@@ -0,0 +1,24 @@
+import scraperYG as scraper
+import pymysql as mysql
+from datetime import datetime, timedelta, date
+import time
+from datetime import datetime, timedelta
+
+
+def ultimo():
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+    cur = mariadb_connection.cursor()
+    sql = "SELECT pagina FROM checker WHERE nombrescraper='syg'"
+    cur.execute(sql)
+    tupla = cur.fetchall()
+    return tupla
+
+
+while True:
+    last=ultimo()[0]
+    pagina=last[0]
+    try:
+        scraper.main(pagina,True)
+    except:
+        print("[SYG] ERROR DE SCRAPER. INICIANDO NUEVAMENTE EN 60 SEGUNDOS")
+        time.sleep(60)
