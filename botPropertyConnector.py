eed4b0dc1a8904009a00e3aba4caaa92c63c6184
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-07-31 19:21:13 -0400

    Update botPropertyConnector.py

--- botPropertyConnector.py
+++ botPropertyConnector.py
@@ -115,7 +115,7 @@ def tasador(client):
     print("Propiedades Check")
 
     tasacion=tb2.calcularTasacionData(client["operacion"].lower(),client["tipo"].lower(),client["lat"],client["lon"],int(client["metros"]),
-                                      int(client["total"]),int(client["dorms"]),int(client["bathss"]),0,props)
+                                      int(client["total"]),int(client["dormitorios"]),int(client["baths"]),0,props)
     print("Tasacion Check")
 
     if tasacion[0]==None:
