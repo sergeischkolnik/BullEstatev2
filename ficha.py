db4a1cb07e29909cb38762d27381a18bf190abfe
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-08-06 17:47:49 -0400

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -378,9 +378,7 @@ def crearFicha(sitio,id,mail,tipoficha):
 
     datospro = []
     if pro:
-        comunaP = (comuna.replace(' ', '-') + '-metropolitana').lower()
-        listacomunas=[]
-        listacomunas.append(comunaP)
+        listacomunas.append(comuna)
         propsP = reportes.from_portalinmobiliario(tipo, regionP,listacomunas, True)
         propsY = reportes.from_yapo(tipo, regionY,listacomunas, True, True)
         props = propsP + propsY
