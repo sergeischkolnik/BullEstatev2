59b5286afa0433dc5f91b5634d8af649eed04943
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-09-11 15:44:34 -0300

    cambios

--- scraperTocTocV2.py
+++ scraperTocTocV2.py
@@ -7,6 +7,20 @@ import time
 from bs4 import BeautifulSoup
 import requests
 
+def get_scraped():
+    sql = "select IdBienRaiz from propiedadestoctoc"
+
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='toctoc2')
+
+    cur = mariadb_connection.cursor()
+    cur.execute(sql)
+
+    tupla = cur.fetchall()
+    mariadb_connection.close()
+    scraped = []
+    for name in tupla:
+        scraped.append(name[0])
+    return scraped
 
 def get_proxy():
     res = requests.get('https://free-proxy-list.net/', headers={'User-Agent':'Mozilla/5.0'})
