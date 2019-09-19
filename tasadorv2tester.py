18e39c0b75fc814dfa75f0074c98666f4235a725
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-09-19 12:49:56 -0300

    Create tasadorv2tester.py

---
+++ tasadorv2tester.py
@@ -0,0 +1,21 @@
+import tasadorv2
+import reportes
+import uf
+
+tipo="departamento"
+
+comunas=['']
+count=0
+deltaprice=[]
+
+data=reportes.from_portalinmobiliario(tipo,"metropolitana",comunas,False)
+datalen=len(data)
+
+for prop in data:
+    count+=1
+    precio,confianza,nrProps,links,venta,g = tasadorv2.calcularTasacionData(prop[3],prop[4],prop[10],prop[11],prop[8],prop[9],prop[6],prop[7],prop[8],data)
+    realprice=prop[5]/uf.getUf()
+    difprice=abs(realprice-precio)/realprice
+    deltaprice.append(difprice)
+    difpriceprint=int(difprice*100)
+    print(str(count)+"/"+str(datalen)+"----------"+str(difpriceprint)+"%")
