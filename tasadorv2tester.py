0ac2f6870a23bc0d65125bd163a1061aac99791e
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-09-19 13:40:47 -0300

    Update tasadorv2tester.py

--- tasadorv2tester.py
+++ tasadorv2tester.py
@@ -15,14 +15,26 @@ datalen=len(data)
 
 for prop in data:
     count+=1
-    print(prop[3], prop[4], prop[10], prop[11], prop[8], prop[9], prop[6], prop[7], prop[12])
+    #print(prop[3], prop[4], prop[10], prop[11], prop[8], prop[9], prop[6], prop[7], prop[12])
     precio,confianza,nrProps,links,venta,g = tasadorv2.calcularTasacionData(prop[3],prop[4],prop[10],prop[11],prop[8],prop[9],prop[6],prop[7],prop[12],data)
-    print(precio)
+    #print(precio)
     realprice=prop[5]/ufn
     difprice=abs(realprice-precio)/realprice
-    deltaprice.append(difprice)
+    array=[]
+    array.append(difprice)
+    array.append(nrProps)
+    array.append(confianza)
+    deltaprice.append(array)
     difpriceprint=int(difprice*100)
-    print(str(count)+"/"+str(100)+"----------"+str(difpriceprint)+"% ----------- Realprice: "+str(realprice)+" PredictedPrice: "+str(precio))
-    if count>99:
+    #print(str(count)+"/"+str(100)+"----------"+str(difpriceprint)+"% ----------- Realprice: "+str(realprice)+" PredictedPrice: "+str(precio))
+    if count>199:
         break
 print(sum(deltaprice)/len(deltaprice))
+deltaprice=sorted(deltaprice, key=lambda x:x[0],reverse=True)
+
+count=0
+for i in deltaprice:
+    print(i)
+    count+=1
+    if count<30:
+        break
