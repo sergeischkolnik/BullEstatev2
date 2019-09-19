fef1fa459bfd0dc1747c88956abfe449ed5de0ce
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-09-19 12:59:52 -0300

    Update tasadorv2tester.py

--- tasadorv2tester.py
+++ tasadorv2tester.py
@@ -11,11 +11,9 @@ deltaprice=[]
 
 data=reportes.from_portalinmobiliario(tipo,"metropolitana",comunas,False)
 datalen=len(data)
-data=random.shuffle(data)
-print(data[1])
-data2=data[:100]
 
-for prop in data2:
+
+for prop in data:
     count+=1
     precio,confianza,nrProps,links,venta,g = tasadorv2.calcularTasacionData(prop[3],prop[4],prop[10],prop[11],prop[8],prop[9],prop[6],prop[7],prop[8],data)
     realprice=prop[5]/uf.getUf()
@@ -23,4 +21,6 @@ for prop in data2:
     deltaprice.append(difprice)
     difpriceprint=int(difprice*100)
     print(str(count)+"/"+str(100)+"----------"+str(difpriceprint)+"% ----------- Realprice: "+str(realprice)+" PredictedPrice: "+str(precio))
+    if count>99:
+        break
 print(sum(deltaprice)/len(deltaprice))
