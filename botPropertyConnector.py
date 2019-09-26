aa359c0f9c80863236ff218986dc930817f60265
Demian-bullestate <demianschkolnik@gmail.com>
2019-09-26 11:48:30 -0300

    Update botPropertyConnector.py

--- botPropertyConnector.py
+++ botPropertyConnector.py
@@ -3,7 +3,7 @@ import botPropertySelect
 import botPropertyMain
 import botPropertySelect
 import ficha
-import reporteshuberV1 as reportes
+import reportesHuberV1 as reportes
 import pymysql as mysql
 import uf
 import tasadorbot2 as tb2
