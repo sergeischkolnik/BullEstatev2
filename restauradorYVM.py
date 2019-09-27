4529da383df13cde1fab1c360df180506c2669db
Demian-bullestate <demianschkolnik@gmail.com>
2019-09-27 16:41:53 -0300

    Update restauradorYVM.py

--- restauradorYVM.py
+++ restauradorYVM.py
@@ -8,21 +8,17 @@ from datetime import datetime, timedelta
 def ultimo():
     mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
     cur = mariadb_connection.cursor()
-    sql = "SELECT operacion,tipo,region,pagina FROM checker WHERE nombrescraper='syvm'"
+    sql = "SELECT pagina FROM checker WHERE nombrescraper='syvm'"
     cur.execute(sql)
     tupla = cur.fetchall()
     return tupla
 
 
 while True:
-    last=ultimo()[0]
-    print("LAST:" + str(last))
-    tipo=last[1]
-    op=last[0]
-    region=last[2]
-    pagina=last[3]
+    pagina=ultimo()[0]
+    print(pagina)
     try:
-        scraper.main(tipo,op,region,pagina,True)
+        scraper.main(tipo="",op="",region="",pagina,True)
     except:
         print("[SYVM] ERROR DE SCRAPER. INICIANDO NUEVAMENTE EN 60 SEGUNDOS")
         time.sleep(60)
