bc176598c739f3d10a9a7d3035ec8362109d6397
Demian-bullestate <demianschkolnik@gmail.com>
2019-09-27 16:48:08 -0300

    Create restauradorYAM.py

---
+++ restauradorYAM.py
@@ -0,0 +1,26 @@
+import scraperYAM as scraper
+import pymysql as mysql
+from datetime import datetime, timedelta, date
+import time
+from datetime import datetime, timedelta
+
+
+def ultimo():
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+    cur = mariadb_connection.cursor()
+    sql = "SELECT pagina FROM checker WHERE nombrescraper='syam'"
+    cur.execute(sql)
+    tupla = cur.fetchall()
+    return tupla
+
+
+while True:
+    pagina=ultimo()[0][0]
+    print(pagina)
+
+    try:
+        scraper.main(tipoRec="departamento",operacionRec="arriendo", regionRec="metropolitana",pagRec=pagina,isRecovery=False,ocr=None)
+
+    except:
+        print("[SYVM] ERROR DE SCRAPER. INICIANDO NUEVAMENTE EN 60 SEGUNDOS")
+        time.sleep(60)
