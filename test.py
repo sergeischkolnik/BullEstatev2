78163e102a8baca3b07a3b5b1ced349e77925bf5
Demian Schkolnik <demianschkolnik@gmail.com>
2019-10-01 16:15:37 -0300

    test

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ session = HTMLSession()
 newLink="""https://www.portalinmobiliario.com/venta/departamento/las-condes-metropolitana/5053667-el-golf-escuela-militar-inversion-asegurada-uda#position=18&type=item&tracking_id=094e9246-b07c-4a67-a0f0-c4efe0a8f83e"""
 r = session.get(newLink)
 rtext=r.text
+print(rtext)
 if "profile-info-phone-value" in rtext:
     print("si")
 else:
