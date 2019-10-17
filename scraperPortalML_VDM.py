8733a070956b7247d968c6eaeeb99fb1c73764fd
Demian-bullestate <demianschkolnik@gmail.com>
2019-10-17 01:14:58 -0300

    Update scraperPortalML_VDM.py

--- scraperPortalML_VDM.py
+++ scraperPortalML_VDM.py
@@ -377,18 +377,25 @@ def scrap(linkList,region,operacion,comuna,tipo,dorms,baths,hoja):
         minMeters = 0
         estacionamientos = 0
         bodegas = 0
+        minMetersFound = maxMetersFound = estacionamientosFound = bodegasFound = False
+
         try:
             for element in htmlArray:
-                if "Superficie total" in element:
+                if "Superficie total" in element and not maxMetersFound:
                     maxMeters = int(float(element.split('span')[1][1:-5]))
-                elif "Superficie útil" in element:
-                    minMeters = int(float(element.split('span')[1][1:-5]))
-                elif "Estacionamientos" in element:
-                    estacionamientos = int(float(element.split('span')[1].replace('<','').replace('>','').replace('/','')))
-                elif "Bodegas" in element:
-                    bodegas = int(float(element.split('span')[1].replace('<','').replace('>','').replace('/','')))
-        except Exception as err:
-            error(link, "Error en bloque de info")
+                    maxMetersFound = True
+                elif "Superficie útil" in element and not minMetersFound:
+                    minMeters = element.split('span')[1][1:-5]
+                    minMetersFound = True
+                elif "Estacionamientos" in element and not estacionamientosFound:
+                    estacionamientos = int(
+                        float(element.split('span')[1].replace('<', '').replace('>', '').replace('/', '')))
+                    estacionamientosFound = True
+                elif "Bodegas" in element and not bodegasFound:
+                    bodegas = int(float(element.split('span')[1].replace('<', '').replace('>', '').replace('/', '')))
+                    bodegasFound = True
+        except:
+            error(link, "Error en superficies, estacionamientos o bodegas.")
             continue
 
         if maxMeters != 0 and minMeters == 0:
