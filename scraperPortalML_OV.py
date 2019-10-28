638a04b4f4a0dfe1facb0cffdc46fb35eaa18853
Demian Schkolnik <demianschkolnik@gmail.com>
2019-10-28 09:30:11 -0300

    Update scraperPortalML_OV.py

--- scraperPortalML_OV.py
+++ scraperPortalML_OV.py
@@ -551,7 +551,6 @@ def main():
                                    dormitorio+"/valparaiso/"+bano+"_desde_"+str(page)
                             print(link)
                             request = requests.get(link, headers =headers)
-                            print(request)
 
                             try:
                                 tree = html.fromstring(request.content)
