c9321524ef8df2d6cdf811c2c1b3bb20c3e17695
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-11-04 10:59:56 -0300

    Update linksCaptador.py

--- linksCaptador.py
+++ linksCaptador.py
@@ -6,7 +6,7 @@ yesterday = datetime.now() - timedelta(days=10)
 yesterday=datetime.date(yesterday)
 
 def main():
-    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='yapo')
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
     cur = mariadb_connection.cursor()
     sql ='select id2,fechapublicacion,fechascrap,operacion,tipo,precio,dormitorios,banos,metrosmin,metrosmax,lat,lon,' \
          'estacionamientos,link from duenos inner join portalinmobiliario where ' \
