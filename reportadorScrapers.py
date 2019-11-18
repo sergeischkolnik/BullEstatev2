d00a5e0c93f680354fb4ce11e81886187f80e8c8
Demian-bullestate <demianschkolnik@gmail.com>
2019-11-18 10:53:18 -0300

    Update reportadorScrapers.py

--- reportadorScrapers.py
+++ reportadorScrapers.py
@@ -4,20 +4,38 @@ from datetime import datetime, timedelta, date
 
 reportar_a = ["sergei@bullestate.cl"]
 
-tipos=["casa","comercial","departamento","parcela","oficina","industrial","agricola","terreno-en-construccion","bodega","estacionamiento"]
+tipos=["casa","departamento","otro"]
 operaciones = ["venta","arriendo"]
-yesterday = datetime.now() - timedelta(days=10)
+regiones = ["metropolitana","valparaiso","otra"]
+yesterday = datetime.now() - timedelta(days=1)
 yesterday=datetime.date(yesterday)
 
 def main():
     for tipo in tipos:
         for operacion in operaciones:
-            sql = "SELECT COUNT('id') from portalinmobiliario where tipo='"+str(tipo)+"' and operacion='"+operacion+"' and fechascrap>='"+str(yesterday)+"'"
-            mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
-            cur = mariadb_connection.cursor()
-            cur.execute(sql)
-            elem = cur.fetchall()
-            print(tipo+","+operacion+"->"+str(elem))
+            for region in regiones:
+                sql = "SELECT COUNT('id') from portalinmobiliario where "
+
+                if tipo != "otros":
+                    sql += "tipo='"+str(tipo)+"' and "
+                else:
+                    sql += "tipo!=departamento and tipo!=casa and "
+
+
+                sql += "operacion='"+operacion+"' and "
+
+                sql += "fechascrap>='"+str(yesterday)+"' and "
+
+                if region != "otra":
+                    sql += "region='"+str(region)+"'"
+                else:
+                    sql += "region!=metropolitana and region!=valparaiso"
+
+                mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bullestate')
+                cur = mariadb_connection.cursor()
+                cur.execute(sql)
+                elem = cur.fetchall()
+                print(tipo+","+operacion+"->"+str(elem[0][0]))
 
 if __name__ == "__main__":
     while(True):
