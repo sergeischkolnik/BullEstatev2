db8b051f400a17b16102ccb8be677488f7570840
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-12-12 14:04:43 -0300

    Update variacionPrecios.py

--- variacionPrecios.py
+++ variacionPrecios.py
@@ -49,7 +49,8 @@ def main():
                     for propiedad in propiedades:
                         if propiedad[3]>0 and propiedad[4]>0 and propiedad[2]>0:
                             propiedad=list(propiedad)
-                            propiedades.sort(key=lambda propiedad:propiedad[0])
+                            sorted(propiedad, key=lambda d: map(int, d.split('-')))
+                            #propiedades.sort(key=lambda propiedad:propiedad[0])
                             promedio=(propiedad[3]+propiedad[4])/2
                             preciometro=propiedad[2]/promedio
                             datos.append([propiedad[1],preciometro])
