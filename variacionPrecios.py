c3468a35411d2319cfe11e152a98269dcd4f431c
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-12-12 13:18:48 -0300

    Update variacionPrecios.py

--- variacionPrecios.py
+++ variacionPrecios.py
@@ -46,13 +46,18 @@ def main():
                     propiedades=obtenerPropiedades(comuna,i,j,k)
                     propiedades=list(propiedades)
                     datos=[]
+                    import datetimenow=datetime.date.today()
+                    now.isoweekday()
+                    days=[now+datetime.timedelta(days=x) for x in range(30)]
+                    days
                     for propiedad in propiedades:
                         if propiedad[3]>0 and propiedad[4]>0 and propiedad[2]>0:
                             propiedad=list(propiedad)
+                            propiedades.sort(days, key=lambda propiedad:propiedad[0])
                             promedio=(propiedad[3]+propiedad[4])/2
                             preciometro=propiedad[2]/promedio
                             datos.append([propiedad[1],preciometro])
-                            propiedades.sort(key=lambda propiedad:propiedad[0])
+
                     for d in datos:
                         print(d)
 
