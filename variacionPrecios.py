0ea6c006ffc51e407bf07cfe81935f58b5f5ecea
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2019-12-13 11:03:06 -0300

    Update variacionPrecios.py

--- variacionPrecios.py
+++ variacionPrecios.py
@@ -69,6 +69,11 @@ def main():
                             #print(datos[n])
                     avgdatos = {}
                     for w,v in datos.items():
+                        for v in sorted(key=lambda datos:datos[n]):
+                            fiveperc= 0.05*len(v)
+                            inicio[]=datos[:fiveperc]
+                            obtenerfin[]=datos[fiveperc:]
+
                         avgdatos[w] = sum(v)/ float(len(v))
 
                     avgdatos["comuna"]=comuna
