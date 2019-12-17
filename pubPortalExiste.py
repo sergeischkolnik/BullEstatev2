5f7250b07e6da0d74e1771a1c2e172f952ea2d0d
Demian-bullestate <demianschkolnik@gmail.com>
2019-12-17 15:22:38 -0300

    Update pubPortalExiste.py

--- pubPortalExiste.py
+++ pubPortalExiste.py
@@ -2,8 +2,6 @@ from lxml import html
 import requests
 import agentCreator
 import time
-import reportes
-import botPropertyConnector
 
 
 def publicacionExiste(link):
@@ -13,27 +11,18 @@ def publicacionExiste(link):
     except:
         return False
 
-    paginas = tree.xpath('//*[@id="wrapper"]/section/div/div[1]/div/h1')
+    paginas = tree.xpath('//*[@id="short-desc"]/div/header/h1')
     if len(paginas)!=0:
-        if "Esta propiedad ya no" in paginas[0].text:
-            return False
-        else:
-            return True
-    else:
         return True
+    else:
+        return False
     time.sleep(0.3)
 
 def main():
-    link='http://www.portalinmobiliario.com/venta/departamento/santiago-metropolitana/4925265-av-blanco-encalada-almirante-latorre-uda?tp=2&op=1&iug=441&ca=2&ts=1&mn=1&or=&sf=0&sp=0&at=0&i=86'
+    link='https://www.portalinmobiliario.com/venta/departamento/santiago-metropolitana/5246104-metro-union-latinoamericana-uda'
     avaible=publicacionExiste(link)
     print(avaible)
-    id=botPropertyConnector.obtenerIdConLink(link,"www.portalinmobiliario.com")
-    id=id[0]
-    print(id)
-    prop=reportes.precio_from_portalinmobiliario(id)
-    prop=prop[0]
-    for p in prop:
-        print(p)
+
 
 if __name__ == '__main__':
     main()
