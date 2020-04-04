8ce80d92e047b8b911346a10c56107c40f078ca5
sergeischkolnik <35017747+sergeischkolnik@users.noreply.github.com>
2020-04-04 13:09:40 -0300

    Create aux2final(3)comunasv2FINAL.final.py

---
+++ aux2final(3)comunasv2FINAL.final.py
@@ -0,0 +1,17 @@
+import pymysql as mysql
+
+def obtenedor():
+
+    mariadb_connection = mysql.connect(user='root', password='sergei', host='127.0.0.1', database='bot')
+    cur = mariadb_connection.cursor()
+    sql = "SELECT id,link from portalinmobiliario limit 100"
+    cur.execute(sql)
+    list = cur.fetchall()
+    return list
+
+def main():
+    lista=obtenedor()
+    for row in lista:
+        link=row[1]
+        comuna=link.split("/")[5].replace("-"," ").capitalize()
+        print (comuna)
